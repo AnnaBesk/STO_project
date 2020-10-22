aaa9d9f32487514eaba9af58307a2f5bb8c7fa34
Anna <annabesk05@gmail.com>
2020-10-22 18:05:49 +0300

    Check var

--- Вычисления.py
+++ Вычисления.py
@@ -1,2 +1,8 @@
-print('Всё работает')
-print('TOK')
+import math
+v=int(input("Введите скорость"))
+c=300000000 #скорость света в м/с
+b=math.pow(v,2)/math.pow(c,2) #бетта
+y=1/math.sqrt(1-b)# гамма
+t=1 #1 секунда в неподвижной СО
+t_=t/y
+print(round(t_, 5))
